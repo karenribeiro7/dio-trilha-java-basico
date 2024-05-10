18fdac8be1220f76692ec2b734659a335c01ea19
karenribeiro7 <kevr@discente.ifpe.edu.br>
2024-05-10 02:37:47 -0300

    finalização do código para desafio da sintaxe java, solicitando dados ao usuario por terminal e exibindo mensagem com os dados informados.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       int numero;
+       String agencia, nomeCliente;
+       double saldo;
+       
+       Scanner scanner = new Scanner(System.in);
+
+       System.out.println("Digite o número da conta: "); 
+       numero = scanner.nextInt();
+
+       System.out.println("Digite o número da agência: ");
+       agencia = scanner.next();
+
+       System.out.println("Digite o saldo da conta: ");
+       saldo = scanner.nextDouble();
+       
+       System.out.println("Digite o nome do cliente: ");
+       nomeCliente = scanner.next();
+
+       System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta "+ numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+
     }
 }
